0ea1414ed82fe7c2cb115faa468f50df8c4b3034
vichoko <vicenteoyanedel@gmail.com>
2017-05-01 18:54:45 -0300

    segmentGenerator listo

--- src/SegmentGeneration/SegmentGenerator.java
+++ src/SegmentGeneration/SegmentGenerator.java
@@ -1,6 +1,5 @@
 package SegmentGeneration;
 
-import java.io.File;
 import java.util.Random;
 /**
  * Segmentos almacenados como tupla (x1,y1, x2,y2)
@@ -34,12 +33,10 @@ public class SegmentGenerator {
     }
 
     protected void GenerateSegments(){
-        SegmentDispatcher dispatcher = new SegmentDispatcher(Long.toString(System.currentTimeMillis()));
+        SegmentDispatcher dispatcher = new SegmentDispatcher(Long.toString(System.currentTimeMillis()) + ".txt");
         Random uniRand = new Random();
         NormalRandom normalRand = new NormalRandom(XMAX/2, XMAX/7);
-
         double x1, y1, x2, y2;
-
 
         if (distr == EDistribution.UNIFORM){
             // Generar N segmentos con distribucion de coordenadas uniformes. Balance dependiendo de a.
@@ -47,42 +44,49 @@ public class SegmentGenerator {
             for (int i = 0; i < a*n ; i++){
                 x1 = uniRand.nextDouble()*XMAX;
                 y1 = uniRand.nextDouble()*YMAX;
+
                 x2 = x1;
                 y2 = uniRand.nextDouble()*YMAX;
-                dispatcher.sendSegment(x1,y1,x2,y2);
+                dispatcher.saveSegment(x1, y1, x2, y2);
             }
             // Generar Horizontales
-            for (int i = 0; i < (a-1)*n; i++){
+            for (int i = 0; i < (1-a)*n; i++){
                 x1 = uniRand.nextDouble()*XMAX;
                 y1 = uniRand.nextDouble()*YMAX;
+
                 x2 = uniRand.nextDouble()*YMAX;
                 y2 = y1;
-                dispatcher.sendSegment(x1,y1,x2,y2);
+                dispatcher.saveSegment(x1, y1, x2, y2);
             }
 
         } else {
             // Generar N segmentos con distribucion de coordenadas uniformes. Excepto la coordenada x de los verticales debe tener normal. Balance dependiendo de a.
             //Genero los verticales
             for (int i = 0; i < a*n ; i++){
-                x1 = normalRand.getGaussian()*XMAX;
+                x1 = normalRand.getGaussian();
                 y1 = uniRand.nextDouble()*YMAX;
+
                 x2 = x1;
                 y2 = uniRand.nextDouble()*YMAX;
-                dispatcher.sendSegment(x1,y1,x2,y2);
+                dispatcher.saveSegment(x1, y1, x2, y2);
             }
             // Generar Horizontales
-            for (int i = 0; i < (a-1)*n; i++){
+            for (int i = 0; i < (1-a)*n; i++){
                 x1 = uniRand.nextDouble()*XMAX;
                 y1 = uniRand.nextDouble()*YMAX;
+
                 x2 = uniRand.nextDouble()*YMAX;
                 y2 = y1;
-                dispatcher.sendSegment(x1,y1,x2,y2);
+                dispatcher.saveSegment(x1, y1, x2, y2);
             }
-
         }
+        dispatcher.close();
     }
 
     public static void main(String[] args){
+        SegmentGenerator generator = new SegmentGenerator((int) Math.pow(2,9) , EDistribution.NORMAL, 0.5);
 
+        generator.GenerateSegments();
+        System.out.println(" done");
     }
 }
