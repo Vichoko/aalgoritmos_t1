fd46a37b8b6172870896b21a1ebf1b062064b474
cfierro94 <constanza.fierro94@gmail.com>
2017-05-23 15:18:06 -0400

    parece que esto es lo tuyo..

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -6,6 +6,9 @@ public final class Constants {
         UNIFORM, NORMAL
     }
 
+    public static final boolean DEBUG = true;
+    public static boolean DEBUG_PM_TRIGGER = true;
+
     // segment.Segment Generator
     public static final int Y_MAX = 100;
     public static final int X_MAX = 100;
@@ -19,8 +22,12 @@ public final class Constants {
 	// Bytes of RAM
     public static final int M = 1024;
     // Bytes of page
-    public static final int B = 256;
+    public static final int B = 128;
 
     // Total segments
     public static final int TOTAL_SEGMENTS = (int) Math.pow(2,10);
+
+    //TESTING
+    public static int IO_COUNTER = 0;
+
 }
--- src/utils/Slab.java
+++ src/utils/Slab.java
@@ -4,14 +4,28 @@ import segment.Segment;
 
 public class Slab {
     public int initialOffset, finalOffset;
+    public int initialIndex, finalIndex;
     public double initX, finalX;
 
     public int verticalSegmentsNumber;
     private Segment lastVerticalSegment;
 
-    public Slab(int initialOffset, int finalOffset, double initX, double finalX, int vsn, Segment lastVerticalSegment) {
+    /**
+     *
+     * @param initialOffset Where to star reading in the file
+     * @param finalOffset Where to stop reading in the file
+     * @param initialIndex Where to start reading in the first segments page
+     * @param finalIndex Where to stop reading in the last segments page.
+     * @param vsn Number of vertical segments in the slab
+     * @param lastVerticalSegment Last vertical segment inside the slab
+     * @param initX X coord where this slab starts
+     * @param finalX X coord where this slab finish
+     */
+    public Slab(int initialOffset, int finalOffset, int initialIndex, int finalIndex, int vsn, Segment lastVerticalSegment, double initX, double finalX) {
         this.initialOffset = initialOffset;
+        this.initialIndex = initialIndex;
         this.finalOffset = finalOffset;
+        this.finalIndex = finalIndex;
         this.initX = initX;
         this.finalX = finalX;
         this.verticalSegmentsNumber = vsn;
